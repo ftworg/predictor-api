4e45f15606f635b03858739503096b0dfcefbdfc
Aswin <ftworgtech@gmail.com>
2020-07-05 00:26:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require("express");
-const app = express();
-const admin = require("firebase-admin");
 const startupScripts = require("./startup/pre_startup");
-
-require("express-async-errors");
-require("./startup/auth")(admin);
-require("./startup/routes")(app);
 
 // app.use(express.static("/tmp/"));
 
-const PORT = process.env.PORT || 5000;
 startupScripts().then(()=>{
+    const PORT = process.env.PORT || 5000;
+    const express = require("express");
+    const app = express();
+    const admin = require("firebase-admin");
+    require("express-async-errors");
+    require("./startup/auth")(admin);
+    require("./startup/routes")(app);
     app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
 }).catch((e)=>{
     throw new Error(e);
