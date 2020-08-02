685deebd24550417fbb3278f1b463bd1aafaaf39
Aswin <ftworgtech@gmail.com>
2020-08-02 10:57:10 +0530

    model info update

--- routes/model.js
+++ routes/model.js
@@ -2,20 +2,20 @@ const express = require("express");
 const router = express.Router();
 // const cache = require("../middlewares/cache");
 const auth = require("../middlewares/auth");
-const modelinfo = global.ASSETS['001']['models'];
+// const modelinfo = global.ASSETS['001']['models'];
+const datastoreUtils = require("../cloud/datastoreUtils");
 
 router.get("/", auth, async (req, res) => {
+  let modelinfo = await datastoreUtils.getGenericObject('tenant001','ModelMetadata',{
+    "Tenant": 'tenant001'
+  });
   let stamp = Math.floor(modelinfo["time"]);
   let time = new Date(stamp * 1000);
-  //   res.send({
-  //       "date": time.getDate(),
-  //       "month": time.getMonth(),
-  //       "year": time.getFullYear(),
-  //       "hours": time.getHours(),
-  //       "minutes": time.getMinutes(),
-  //       "seconds": time.getSeconds()
-  //   });
-  res.send(time);
+  res.send({
+    "time": time,
+    "lastEntryDate": JSON.parse(modelinfo["date"]),
+    "version": modelinfo["ver"]
+  });
 });
 
 module.exports = router;
